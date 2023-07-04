9cfafda4051f7c7a945cf6fc2373bcb4be8fde5e
thinh.bachthai <thinh.bachthai@email.com>
2023-07-04 15:51:52 +0900

    chore : fix comment

--- parameter.ts
+++ parameter.ts
@@ -4,23 +4,39 @@ export interface AppParameter {
   env?: Environment;
   envName: string;
   s3BucketName: string;
+  
+  //vpc
+  vpcName?: string;
+  vpcMaxAzs?: number;
+  vpcNatGateways?: number;
+  natGatewaySubnetCidr? : number;
+  ec2SubnetCidr?: number;
+  rdsSubnetCidr?: number;
 }
 
-// Example for Development
+// Example for Develop Development
 export const devParameter: AppParameter = {
   envName: 'Development',
   env: { account: '123456789012', region: 'ap-northeast-1' },
   s3BucketName: 'thinhbt-dev-s3-bucket',
+  //vpc
+  vpcName: 'thinhbt-vpc',
+  vpcMaxAzs: 2,
+  vpcNatGateways: 1,
+  natGatewaySubnetCidr: 24,
+  ec2SubnetCidr: 24,
+  rdsSubnetCidr: 24,
+  
 };
 
-// Example for Staging
+// Example for Staging Development
 export const stagParameter: AppParameter = {
   envName: 'Staging',
   env: { account: '123456789012', region: 'ap-southeast-2' },
   s3BucketName: 'thinhbt-stg-s3-bucket',
 };
 
-// Example for Pipeline Deployment
+// Example for Product Deployment
 export const prodParameter: AppParameter = {
   envName: 'Production',
   env: { account: '123456789012', region: 'ap-northeast-1' },
